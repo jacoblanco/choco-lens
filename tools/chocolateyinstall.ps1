0a5434b8981da815c6d857f152838acf7fbe85b0
virtualex <info@ithinkvirtual.com>
2020-09-15 11:17:45 -0400

    Updated to 3.6.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference	= 'Stop';
 $toolsDir	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName	= 'lens'
 $fileType	= 'exe'
-$url	= 'https://github.com/lensapp/lens/releases/download/v3.5.3/Lens-Setup-3.5.3.exe'
+$url	= 'https://github.com/lensapp/lens/releases/download/v3.6.0/Lens-Setup-3.6.0.exe'
 $softwareName	= 'Lens*'
-$checksum	= 'E50EBCD24A20B4248870C26938540601F0C3EF3F51E331586F72A3F6BCFDF6EB'
+$checksum	= '2E07399ECC3A5C2C2F506F7E77AF199F9117688769580537E7D1772EFE394C37'
 $checksumType	= 'sha256'
 $validExitCodes		= @(0, 3010, 1605, 1614, 1641)
 
